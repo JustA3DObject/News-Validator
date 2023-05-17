ba206a2765ff13499472e3f79ab883b9da801286
Aayush <just.another.3d.object@gmail.com>
2023-05-17 19:58:31 +0530

    Made lists of title and link

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import requests
+import openpyxl
 from bs4 import BeautifulSoup
+
+# excel = openpyxl.Workbook()
+# sheet = excel.active
+# sheet.title = 'HindustanTimes World News'
+# print(excel.sheetnames)
 
 page_link = []
 articles_list = []
+title_list = []
+link_list = []
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
            "X-Amzn-Trace-Id": "Root=1-62d8036d-2b173c1f2e4e7a416cc9e554", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -10,7 +18,7 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 
 
 def list_maker():
-    for i in range(1, 51):
+    for i in range(1, 4):
         page_link = (f'https://www.hindustantimes.com/world-news/page-{i}')
 
         i = i+1
@@ -35,9 +43,9 @@ list_maker()
 
 for article in articles_list:
 
-    title = article.find('h3', class_='hdg3').a.text
+    title = article.find('h3', class_='hdg3').a.get_text()
+    title = title.strip()
     link = "https://www.hindustantimes.com/" + article.h3.find('a').get('href')
 
-    print(title, link)
-
-# print(len(articles_list))
+    title_list.append(title)
+    link_list.append(link)
