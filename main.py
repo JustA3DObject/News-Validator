b5ea790c3224c991263147b912dd03b2a11dbe1d
Aayush <just.another.3d.object@gmail.com>
2023-05-21 20:19:57 +0530

    Iterating all links in link list to fetch details

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import requests
 import openpyxl
 from bs4 import BeautifulSoup
-import re
 
 # excel = openpyxl.Workbook()
 # sheet = excel.active
 # sheet.title = 'HindustanTimes World News'
 # print(excel.sheetnames)
+# sheet.append(['Title', 'Text', 'Subject', 'Date'])
 
 page_link = []
 articles_list = []
 title_list = []
 link_list = []
+body = ""
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
            "X-Amzn-Trace-Id": "Root=1-62d8036d-2b173c1f2e4e7a416cc9e554", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -44,30 +45,29 @@ list_maker()
 
 for article in articles_list:
 
-    # title = article.find('h3', class_='hdg3').a.get_text()
-    # title = title.strip()
     link = "https://www.hindustantimes.com/" + article.h3.find('a').get('href')
-
-    # title_list.append(title)
     link_list.append(link)
 
-try:
-    page = requests.get(link_list[0], headers=headers)
-    page.raise_for_status()
+for i in range(len(link_list)):
+    try:
+        page = requests.get(link_list[i], headers=headers)
+        page.raise_for_status()
 
-    if page.status_code == 200:
-        soup1 = BeautifulSoup(page.text, "html.parser")
-        soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+        if page.status_code == 200:
+            soup1 = BeautifulSoup(page.text, "html.parser")
+            soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
 
-    content = soup2.find('div', id="dataHolder")
+        content = soup2.find('div', id="dataHolder")
 
-    title = content.find('h1', class_='hdg1').get_text().strip()
-    title = ' '.join(title.split())
-    date_time = content.find('div', class_='dateTime').get_text().strip()
+        title = content.find('h1', class_='hdg1').get_text().strip()
+        title = ' '.join(title.split())
 
-    for item in content.find_all('p'):
-        body = ' '.join(item.find_all(text=True)).strip()
+        date_time = content.find('div', class_='dateTime').get_text().strip()
+
+        body = [x.get_text() for x in content.find_all('p')]
+        body = ' '.join(body)
         body = ' '.join(body.split())
 
-except Exception as e:
-    print(e)
+        print(title, date_time, body)
+    except Exception as e:
+        print(e)
