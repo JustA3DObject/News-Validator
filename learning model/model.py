1fcb63c44e7a9a2cd10ea2b93e163ee425e8092c
Aayush <just.another.3d.object@gmail.com>
2023-07-23 00:45:30 +0530

    Added func to stem the content

--- learning model/model.py
+++ learning model/model.py
@@ -23,3 +23,18 @@ Y = news_dataset['TorF']
 
 # print(X)
 # print(Y)
+
+port_stem = PorterStemmer()
+
+
+def stemming(content):
+    stemmed_content = re.sub('[^0-9a-zA-Z]', ' ', content)
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [
+        port_stem.stem(word)
+        for word in stemmed_content
+        if word not in stopwords.words('english')
+    ]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
