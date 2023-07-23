dc14dd3f7b75552fe07e3e89ec7dfe37d0ff8a1e
Aayush <just.another.3d.object@gmail.com>
2023-07-24 00:58:53 +0530

    Changed joblib with pickle

--- learning model/model.py
+++ learning model/model.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score
-import joblib
+from pickle import dump, load
 
 nltk.download('stopwords')
 
