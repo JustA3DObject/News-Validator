9fb1bb22f972fac209415133d6937d355e49892e
Aayush <just.another.3d.object@gmail.com>
2023-07-25 00:46:35 +0530

    Taking the input and stemming it

--- learning model/Predictor.py
+++ learning model/Predictor.py
@@ -3,3 +3,23 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from pickle import load
+
+X = str(input("Enter the news article text here: "))
+
+port_stem = PorterStemmer()
+
+
+def stemming(content):
+    stemmed_content = re.sub('[^0-9a-zA-Z]', ' ', content)
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [
+        port_stem.stem(word)
+        for word in stemmed_content
+        if word not in stopwords.words('english')
+    ]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
+
+
+X_new = [stemming(X)]
