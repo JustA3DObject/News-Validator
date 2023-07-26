f9727944f39f367dcb93ffbd92ebee178d4f8777
Aayush <just.another.3d.object@gmail.com>
2023-07-26 22:05:59 +0530

    Created a class for taking inputs to the ml model

--- predictor/api.py
+++ predictor/api.py
@@ -2,3 +2,10 @@ from fastapi import FastApi
 from pydantic import BaseModel
 import pickle
 import json
+
+app = FastApi()
+
+class model_input(BaseModel):
+    News_Title : str
+    News_Body : str
+    
