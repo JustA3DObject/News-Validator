8802a17acf2c639101a9522148b143b85daaa827
Aayush <just.another.3d.object@gmail.com>
2023-07-26 22:13:37 +0530

    Created an inpoint and taking json input

--- predictor/api.py
+++ predictor/api.py
@@ -6,6 +6,14 @@ import json
 app = FastApi()
 
 class model_input(BaseModel):
+
     News_Title : str
     News_Body : str
-    
+
+news_model = pickle.load(open("model.pkl", "rb"))
+
+@app.post('/news_validator') #In point
+def news_valid(input_parameters : model_input):
+
+    input_data = input_parameters.json
+    input_dictionary = json.loads(input_data)
